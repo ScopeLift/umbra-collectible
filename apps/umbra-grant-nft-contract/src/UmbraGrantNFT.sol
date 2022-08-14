a009a2ca1be10b77848f3820cc6bf6e3d41de0f0
Keating <keating.dev@protonmail.com>
2022-08-14 14:35:44 -0400

    Add collectible NFT

--- apps/umbra-grant-nft-contract/src/UmbraGrantNFT.sol
+++ apps/umbra-grant-nft-contract/src/UmbraGrantNFT.sol
@@ -1,4 +1,67 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract UmbraGrantNFT {}
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import "solmate/tokens/ERC721.sol";
+
+contract UmbraGrantNFT is ERC721, AccessControl, Initializable {
+  error TokenDoesNotExist();
+  error ClaimPeriodClosed();
+
+  /// @dev Unix timestamp when nfts can no longer be minted
+  uint256 public claimPeriodEnd;
+  bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+  /**
+   * @param _name            Name of the NFT
+   * @param _symbol          Symbol of the NFT
+   * @param _claimPeriodEnd  A unix timestamp after which no more NFTs will be minted
+   */
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    uint256 _claimPeriodEnd
+  ) ERC721(_name, _symbol) {
+    claimPeriodEnd = _claimPeriodEnd;
+    _setRoleAdmin(OWNER_ROLE, OWNER_ROLE);
+    _setRoleAdmin(MINTER_ROLE, OWNER_ROLE);
+    _setupRole(OWNER_ROLE, msg.sender);
+  }
+
+  /**
+   * @param _minter  Contract to receive the minter role
+   */
+  function initialize(address _minter) public onlyRole(OWNER_ROLE) initializer {
+    _setupRole(MINTER_ROLE, _minter);
+  }
+
+  function mint(address to, uint256 id) public onlyRole(MINTER_ROLE) {
+    if (block.timestamp > claimPeriodEnd) {
+      revert ClaimPeriodClosed();
+    }
+    _safeMint(to, id);
+  }
+
+  function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    virtual
+    override(ERC721, AccessControl)
+    returns (bool)
+  {
+    return
+      interfaceId == 0x01ffc9a7 || // ERC165 Interface ID for ERC165
+      interfaceId == 0x80ac58cd || // ERC165 Interface ID for ERC721
+      interfaceId == 0x5b5e139f || // ERC165 Interface ID for ERC721Metadata
+      super.supportsInterface(interfaceId);
+  }
+
+  function tokenURI(uint256 id) public view override returns (string memory) {
+    if (_ownerOf[id] == address(0)) {
+      revert TokenDoesNotExist();
+    }
+    return "ipfs://bafybeihjajj3baoiuk2b4ny5uodx6uvvb73c67xkqhrvtckdos6xie4kfa";
+  }
+}
--- apps/umbra-grant-nft-contract/src/test/UmbraGrantNFT.t.sol
+++ apps/umbra-grant-nft-contract/src/test/UmbraGrantNFT.t.sol
@@ -1,12 +1,110 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-import {DSTestPlus} from 'solmate/test/utils/DSTestPlus.sol';
+import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
+import "../UmbraGrantNFT.sol";
 
-contract UmbraGrantNFTTest is DSTestPlus {
-  function setUp() public {}
+abstract contract UmbraNFTBaseSetup is DSTestPlus {
+  UmbraGrantNFT public collectible;
+  address public minterAddr;
+  address public randomAddr;
 
-  function testExample() public {
-    assertTrue(true);
+  constructor() {
+    minterAddr = hevm.addr(0xBEEF);
+    randomAddr = hevm.addr(1);
+    collectible = new UmbraGrantNFT(
+      "Umbra OG Supporter",
+      "UMBRAOG",
+      1692052154
+    );
+  }
+}
+
+abstract contract UmbraNFTIntialized is UmbraNFTBaseSetup {
+  constructor() {
+    collectible.initialize(minterAddr);
+  }
+}
+
+contract UmbraGrantNFTInitializeTest is UmbraNFTBaseSetup {
+  event Initialized(uint8 version);
+
+  function testCalledOnce() public {
+    // Assert contract initialized
+    hevm.expectEmit(true, false, false, true);
+    emit Initialized(1);
+    collectible.initialize(minterAddr);
+  }
+
+  function testFailCalledTwice() public {
+    collectible.initialize(minterAddr);
+    collectible.initialize(minterAddr);
+  }
+
+  function testFailCalledByOtherContract() public {
+    hevm.prank(randomAddr);
+    collectible.initialize(minterAddr);
+  }
+
+  function testOwnerMinterRoleAdmin() public {
+    collectible.revokeRole(collectible.MINTER_ROLE(), minterAddr);
+  }
+}
+
+contract UmbraGrantNFTMintTest is UmbraNFTIntialized {
+  function testFailOwnerMints() public {
+    collectible.mint(randomAddr, 1);
+  }
+
+  function testFailRandomAddrMint() public {
+    hevm.prank(randomAddr);
+    collectible.mint(randomAddr, 1);
+  }
+
+  function testClaimPeriodEnd() public {
+    // After constructor value in base setup
+    hevm.warp(1692062154);
+    hevm.prank(minterAddr);
+    hevm.expectRevert(UmbraGrantNFT.ClaimPeriodClosed.selector);
+    collectible.mint(randomAddr, 1);
+  }
+
+  function testMint() public {
+    hevm.prank(minterAddr);
+    collectible.mint(randomAddr, 1);
+    address owner = collectible.ownerOf(1);
+    assertTrue(owner == randomAddr);
+  }
+}
+
+contract UmbraGrantNFTSupportsInterfaceTest is UmbraNFTIntialized {
+  function testAllInterfacesSupported() public {
+    bool one = collectible.supportsInterface(0x01ffc9a7);
+    assertTrue(one);
+
+    bool two = collectible.supportsInterface(0x80ac58cd);
+    assertTrue(two);
+
+    bool three = collectible.supportsInterface(0x5b5e139f);
+    assertTrue(three);
+
+    bool four = collectible.supportsInterface(type(IAccessControl).interfaceId);
+    assertTrue(four);
+  }
+}
+
+contract UmbraGrantNFTTokenUriTest is UmbraNFTIntialized {
+  function testNoToken() public {
+    hevm.expectRevert(UmbraGrantNFT.TokenDoesNotExist.selector);
+    collectible.tokenURI(1);
+  }
+
+  function testTokenUri() public {
+    hevm.prank(minterAddr);
+    collectible.mint(randomAddr, 1);
+    string memory uri = collectible.tokenURI(1);
+    string
+      memory expectedUri = "ipfs://bafybeihjajj3baoiuk2b4ny5uodx6uvvb73c67xkqhrvtckdos6xie4kfa";
+    assertEq(uri, expectedUri);
   }
 }
