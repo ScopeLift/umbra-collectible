cd9af771edac9bbefa0ed6453058f24118bbda16
Keating <keating.dev@protonmail.com>
2022-08-13 21:44:07 -0400

    Create collectible contract scaffold
    * Add scaffold for contract with test

---
+++ apps/umbra-grant-nft-contract/src/test/UmbraGrantNFT.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+import {DSTestPlus} from 'solmate/test/utils/DSTestPlus.sol';
+
+contract UmbraGrantNFTTest is DSTestPlus {
+  function setUp() public {}
+
+  function testExample() public {
+    assertTrue(true);
+  }
+}
