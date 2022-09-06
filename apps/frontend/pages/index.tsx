d8d79eb463afd3b57da8146d34fe3823bb2a9a2d
Keating <keating.dev@protonmail.com>
2022-09-06 09:25:54 -0600

    Add different address states

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,5 +1,10 @@
+import React, { useCallback, useEffect, useState } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import styled from "styled-components";
+import { useAccount } from "wagmi";
+
+import { useMerkle } from "../contexts/MerkleContext";
+import { keccak256 } from "ethers/lib/utils";
 
 const StyledPage = styled.div`
   display: flex;
@@ -10,12 +15,42 @@ const StyledPage = styled.div`
 `;
 
 export function Index() {
-  return (
-    <StyledPage>
-      <h1>Connect wallet to claim Umbra NFT</h1>
-      <ConnectButton />
-    </StyledPage>
-  );
+  const [proof, setProof] = useState([]);
+  const { address, isConnected } = useAccount();
+  const { merkleTree } = useMerkle();
+  const content = useCallback(() => {
+    if (isConnected && proof.length) {
+      return (
+        <>
+          <h1>Claim your Umbra NFT</h1>
+          <ConnectButton />
+          <button>Claim</button>
+        </>
+      );
+    } else if (isConnected && !proof.length) {
+      return (
+        <>
+          <h1>Your Address does not qualify</h1>
+          <ConnectButton />
+        </>
+      );
+    } else {
+      return (
+        <>
+          <h1>Connect wallet to claim Umbra NFT</h1>
+          <ConnectButton />
+        </>
+      );
+    }
+  }, [isConnected, proof]);
+
+  useEffect(() => {
+    if (address) {
+      setProof(merkleTree.getProof(keccak256(address)));
+    }
+  }, [address, merkleTree]);
+
+  return <StyledPage>{content()}</StyledPage>;
 }
 
 export default Index;
