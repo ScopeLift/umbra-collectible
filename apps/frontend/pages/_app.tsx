d082f2f834be59046fa4dc5e1294ee85ae722ff0
Keating <keating.dev@protonmail.com>
2022-09-06 08:19:56 -0600

    Initial wallet connect

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,6 +1,49 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import './styles.css';
+import { AppProps } from "next/app";
+import Head from "next/head";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  connectorsForWallets,
+  wallet,
+} from "@rainbow-me/rainbowkit";
+import { chain, createClient, configureChains, WagmiConfig } from "wagmi";
+import { infuraProvider } from "wagmi/providers/infura";
+import { publicProvider } from "wagmi/providers/public";
+const { chains, provider, webSocketProvider } = configureChains(
+  [
+    chain.polygon,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" && [
+      chain.rinkeby,
+      chain.goerli,
+    ]),
+  ],
+  [infuraProvider({ apiKey: process.env.INFURA_API_KEY }), publicProvider()]
+);
+
+const { wallets } = getDefaultWallets({
+  appName: "Umbra collectible airdrop",
+  chains,
+});
+
+const appInfo = {
+  appName: "Umbra Collectilble airdrop",
+};
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: "Other",
+    wallets: [wallet.argent({ chains }), wallet.trust({ chains })],
+  },
+]);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+});
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,9 +51,11 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Welcome to frontend!</title>
       </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider appInfo={appInfo} chains={chains}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </>
   );
 }
