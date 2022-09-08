e8fd812d6344bda1f65e9f3d1a4ee2b05d652f6e
Keating <keating.dev@protonmail.com>
2022-09-07 18:35:16 -0600

    Add contract client

---
+++ libs/umbra-grant-contract-client/README.md
@@ -0,0 +1,11 @@
+# umbra-grant-contract-client
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build umbra-grant-contract-client` to build the library.
+
+## Running unit tests
+
+Run `nx test umbra-grant-contract-client` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/umbra-grant-contract-client/src/lib/umbra-grant-contract-client.ts
@@ -0,0 +1,32 @@
+import { ethers } from "ethers";
+import { MerkleDistributor__factory, MerkleDistributor } from "./generated";
+
+export type NetworkConfig = {
+  address: string;
+  chainId: number;
+  name?: string;
+};
+
+export type ClaimArgs = {
+  index: number;
+  account: string;
+  proof: ethers.BytesLike[];
+};
+
+export class UmbraNFTDistributorContract {
+  distributor: MerkleDistributor;
+
+  constructor(
+    networkConfig: NetworkConfig,
+    provider: ethers.providers.Provider | ethers.Signer
+  ) {
+    this.distributor = MerkleDistributor__factory.connect(
+      networkConfig.address,
+      provider
+    );
+  }
+
+  public async claim(args: ClaimArgs) {
+    return await this.distributor.claim(args.index, args.account, args.proof);
+  }
+}
