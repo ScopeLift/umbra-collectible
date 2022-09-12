536fbecaa0d8637f81514e7be94df947660eb040
Keating <keating.dev@protonmail.com>
2022-09-12 01:53:51 -0400

    Add support for more chains

--- apps/frontend/utils/networks.ts
+++ apps/frontend/utils/networks.ts
@@ -1,11 +1,29 @@
 import { isHexString } from "@ethersproject/bytes";
 
 export const networks = {
+  4: {
+    chainId: 4,
+    address: process.env.NEXT_PUBLIC_RINKEBY_CONTRACT_ADDRESS,
+    name: "Rinkeby",
+    blockExplorerUrl: "https://rinkeby.etherscan.io",
+  },
+  5: {
+    chainId: 5,
+    address: process.env.NEXT_PUBLIC_GOERLI_CONTRACT_ADDRESS,
+    name: "Goerli",
+    blockExplorerUrl: "https://goerli.etherscan.io",
+  },
+  137: {
+    chainId: 137,
+    address: process.env.NEXT_PUBLIC_POLYGON_CONTRACT_ADDRESS,
+    name: "Polygon",
+    blockExplorerUrl: "https://polygonscan.com/",
+  },
   80001: {
     chainId: 80001,
     address: process.env.NEXT_PUBLIC_MUMBAI_CONTRACT_ADDRESS,
     name: "PolygonMumbai",
-    blockExplorerUrls: "https://mumbai.polygonscan.com/",
+    blockExplorerUrl: "https://mumbai.polygonscan.com/",
   },
   31337: {
     chainId: 31337,
@@ -27,8 +45,8 @@ export const getEtherscanUrl = (txHashOrAddress: string, chainId: number) => {
       : "tx"
     : "ens";
   const chain = networks[chainId];
-  const networkPrefix = chain?.blockExplorerUrls?.length
-    ? chain?.blockExplorerUrls[0]
+  const networkPrefix = chain?.blockExplorerUrl
+    ? chain?.blockExplorerUrl
     : "https://etherscan.io";
   if (group === "ens") {
     return `${networkPrefix}`;
